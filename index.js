4f0294274d6b8ee884eeb97f35a7f8d63707cd0a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-05 23:53:31 -0600

    using basic Math built in functions in Javascript for completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;//result 62
+//mod = N = dq + r
+//N/d = q for integer division and r is the remainder
+//N = dq + 4;
+//19 = 5*3+4
+const num3 = 19;
+const num4 = 5;
+let mod = num3 % num4;//result = 4
+let random = Math.floor(Math.random() * 10) + 1;
+let max = Math.max(0, 1, 2, 3, 20);
